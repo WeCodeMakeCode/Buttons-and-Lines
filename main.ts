33e35185fbb5236172770c6c3dd9d1eac0d715da
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-05-23 19:59:57 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
 namespace SpriteKind {
     export const Button = SpriteKind.create()
 }
+function set_active_ndx () {
+	
+}
+function set_button_colors () {
+    for (let index = 0; index <= 14; index++) {
+        Color = index + 1
+        create_button(Color)
+    }
+}
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     make_active(active_row, active_col - 1)
 })
@@ -8,61 +17,71 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     make_active(active_row + 1, active_col)
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    ndx = 4 * active_row + active_col
-    b_selected_list[ndx] = !(b_selected_list[ndx])
-    if (b_selected_list[ndx]) {
-        set_border_color(4)
-    } else {
-        set_border_color(5)
-    }
+    b_selected_list[active_ndx] = !(b_selected_list[active_ndx])
+    set_border_to_status(active_ndx)
 })
 function make_active (row: number, col: number) {
-    set_border_color(0)
-    prior_actine_rpw = active_row
-    prior_active_col = active_col
+    prior_active_ndx = 4 * active_row + active_col
     active_row = Math.abs(row) % 4
     active_col = Math.abs(col) % 4
-    set_border_color(5)
+    active_ndx = 4 * active_row + active_col
+    set_border_to_status(prior_active_ndx)
+    set_border_to_status(active_ndx)
 }
 function create_button (pColor: number) {
     a_button = sprites.create(image.create(40, 30), SpriteKind.Button)
     a_button.image.fill(pColor)
-    set_border_color(0)
     buttons.push(a_button)
     b_selected_list.push(false)
+    set_border_to_status(buttons.length - 1)
 }
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     make_active(active_row, active_col + 1)
 })
-function set_border_color (Color: number) {
-    ndx = 4 * active_row + active_col
+function set_border_to_status (ndx: number) {
     if (ndx < buttons.length) {
-        buttons[ndx].image.drawRect(0, 0, 40, 30, Color)
-        buttons[ndx].image.drawRect(1, 1, 38, 28, Color)
+        let ndx = 0
+        if (b_selected_list[ndx]) {
+            set_border_color(active_ndx, 2)
+        } else if (ndx == active_ndx) {
+            set_border_color(active_ndx, 5)
+        } else {
+            set_border_color(ndx, 15)
+        }
     } else {
-        console.logValue("ndx", ndx)
+        console.logValue("Active index", active_ndx)
     }
+}
+function position_buttons () {
+    active_ndx = 0
+    for (let Row = 0; Row <= 3; Row++) {
+        for (let Col = 0; Col <= 3; Col++) {
+            buttons[active_ndx].left = 40 * Col
+            buttons[active_ndx].top = 30 * Row
+            active_ndx += 1
+        }
+    }
+}
+function set_border_color (ndx: number, color: number) {
+    buttons[ndx].image.drawRect(0, 0, 40, 30, color)
+    buttons[ndx].image.drawRect(1, 1, 38, 28, color)
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     make_active(active_row - 1, active_col)
 })
 let a_button: Sprite = null
-let prior_active_col = 0
-let prior_actine_rpw = 0
-let ndx = 0
+let prior_active_ndx = 0
+let Color = 0
+let active_ndx = 0
 let active_col = 0
 let active_row = 0
-let Color = 0
 let b_selected_list: boolean[] = []
 let buttons: Sprite[] = []
 buttons = sprites.allOfKind(SpriteKind.Button)
 b_selected_list = []
 let yellow = 3
-scene.setBackgroundColor(0)
-for (let index = 0; index <= 14; index++) {
-    Color = index + 1
-    create_button(Color)
-}
+scene.setBackgroundColor(15)
+set_button_colors()
 create_button(1)
 buttons[buttons.length - 1].setImage(img`
 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
@@ -96,14 +115,8 @@ buttons[buttons.length - 1].setImage(img`
 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
 `)
-let active_ndx = 0
-for (let Row = 0; Row <= 3; Row++) {
-    for (let Col = 0; Col <= 3; Col++) {
-        buttons[active_ndx].left = 40 * Col
-        buttons[active_ndx].top = 30 * Row
-        active_ndx += 1
-    }
-}
+position_buttons()
 active_row = 0
 active_col = 0
+active_ndx = 0
 make_active(active_row, active_col)
