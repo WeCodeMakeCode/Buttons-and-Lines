78952966d97f042ed122f576be1301bf89227456
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-05-27 12:21:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 namespace SpriteKind {
     export const Button = SpriteKind.create()
 }
+function make_screen_size_sprites () {
+    draw_on_sprite = sprites.create(image.create(160, 120), SpriteKind.Player)
+    draw_on_sprite.image.fill(15)
+    behind_draw_on_sprite = sprites.create(image.create(160, 120), SpriteKind.Player)
+    behind_draw_on_sprite.image.fill(15)
+}
 function create_buttons () {
     buttons = sprites.allOfKind(SpriteKind.Button)
     for (let index2 = 0; index2 <= 15; index2++) {
@@ -15,29 +21,13 @@ function create_buttons () {
         buttons.push(a_button)
     }
 }
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    if_buttons_make_active(sprites.readDataNumber(active_button, "row"), sprites.readDataNumber(active_button, "col") - 1)
-})
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if_buttons_make_active(sprites.readDataNumber(active_button, "row") + 1, sprites.readDataNumber(active_button, "col"))
-})
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (b_state_is_buttons) {
-        sprites.setDataBoolean(active_button, "selected", !(sprites.readDataBoolean(active_button, "selected")))
-        set_state_borders()
-    }
-})
-function if_buttons_make_active (row: number, col: number) {
-    if (b_state_is_buttons) {
-        Row = Math.abs(row) % 4
-        Col = Math.abs(col) % 4
-        active_button = buttons[4 * Row + Col]
-        set_state_borders()
-    }
-}
 function uncover_buttons () {
     behind_draw_on_sprite.z = -2
     draw_on_sprite.z = -1
+}
+function cover_buttons () {
+    behind_draw_on_sprite.z = 1
+    draw_on_sprite.z = 2
 }
 function do_lines_pattern (aSprite: Sprite, colors_string: string) {
     colors_list = colors_string.split("|")
@@ -61,6 +51,26 @@ function do_lines_pattern (aSprite: Sprite, colors_string: string) {
         n = (n + 1) % colors_list.length
     }
 }
+function make_cs_string_from_colors_of_selected_buttons () {
+    cs = ""
+    for (let index22 = 0; index22 <= buttons.length - 1; index22++) {
+        if (sprites.readDataBoolean(buttons[index22], "selected")) {
+            if (index22 == 0) {
+                cs = "" + index22
+            } else {
+                cs = "" + cs + "|" + index22
+            }
+        }
+    }
+}
+function if_buttons_make_active (row: number, col: number) {
+    if (b_state_is_buttons) {
+        Row = Math.abs(row) % 4
+        Col = Math.abs(col) % 4
+        active_button = buttons[4 * Row + Col]
+        set_state_borders()
+    }
+}
 function set_state_borders () {
     for (let value2 of buttons) {
         if (sprites.readDataBoolean(value2, "selected") == true) {
@@ -72,6 +82,22 @@ function set_state_borders () {
         }
     }
 }
+function set_border_color (aSprite: Sprite, aColor: number) {
+    aSprite.image.drawRect(0, 0, 40, 30, aColor)
+    aSprite.image.drawRect(1, 1, 38, 28, aColor)
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if_buttons_make_active(sprites.readDataNumber(active_button, "row"), sprites.readDataNumber(active_button, "col") - 1)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if_buttons_make_active(sprites.readDataNumber(active_button, "row") + 1, sprites.readDataNumber(active_button, "col"))
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (b_state_is_buttons) {
+        sprites.setDataBoolean(active_button, "selected", !(sprites.readDataBoolean(active_button, "selected")))
+        set_state_borders()
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     b_state_is_buttons = !(b_state_is_buttons)
     draw_on_sprite.image.fill(15)
@@ -88,46 +114,20 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if_buttons_make_active(sprites.readDataNumber(active_button, "row"), sprites.readDataNumber(active_button, "col") + 1)
 })
-function make_cs_string_from_colors_of_selected_buttons () {
-    cs = ""
-    for (let index22 = 0; index22 <= buttons.length - 1; index22++) {
-        if (sprites.readDataBoolean(buttons[index22], "selected")) {
-            if (index22 == 0) {
-                cs = "" + index22
-            } else {
-                cs = "" + cs + "|" + index22
-            }
-        }
-    }
-}
-function make_screen_size_sprites () {
-    draw_on_sprite = sprites.create(image.create(160, 120), SpriteKind.Player)
-    draw_on_sprite.image.fill(15)
-    behind_draw_on_sprite = sprites.create(image.create(160, 120), SpriteKind.Player)
-    behind_draw_on_sprite.image.fill(15)
-}
-function set_border_color (aSprite: Sprite, aColor: number) {
-    aSprite.image.drawRect(0, 0, 40, 30, aColor)
-    aSprite.image.drawRect(1, 1, 38, 28, aColor)
-}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if_buttons_make_active(sprites.readDataNumber(active_button, "row") - 1, sprites.readDataNumber(active_button, "col"))
 })
-function cover_buttons () {
-    behind_draw_on_sprite.z = 1
-    draw_on_sprite.z = 2
-}
+let active_button: Sprite = null
+let Col = 0
+let Row = 0
 let n = 0
 let cy = 0
 let cx = 0
 let colors_list: string[] = []
-let draw_on_sprite: Sprite = null
-let behind_draw_on_sprite: Sprite = null
-let Col = 0
-let Row = 0
-let active_button: Sprite = null
 let a_button: Sprite = null
 let buttons: Sprite[] = []
+let behind_draw_on_sprite: Sprite = null
+let draw_on_sprite: Sprite = null
 let b_state_is_buttons = false
 let cs = ""
 cs = ""
